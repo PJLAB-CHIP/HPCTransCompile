50b5340615a1167b2d99cfdc6b353e323149cf56
root <1031903858@qq.com>
2023-12-12 03:25:42 +0000

    del fine_tune.sh

--- qlora/fine_tune.sh
+++
@@ -1,9 +0,0 @@
-python qlora.py \
-    --dataset ../preprocess/data/hpc_train_v1.0.json \
-    --model_name_or_path ../../model/CodeLlama-13b-hf \
-    --learning_rate 0.0001 \
-    --num_train_epochs 10 \
-    --bits 8 \
-    --dataset_format alpaca \
-    --output_dir ./output/hpc_v1.1 \
-    --per_device_train_batch_size 1 \
