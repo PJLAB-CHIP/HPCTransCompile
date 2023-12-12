6df9b1ebbd48de5787f203cf6fa7252808989c5d
root <1031903858@qq.com>
2023-12-12 03:24:08 +0000

    scripts/

---
+++ qlora/scripts/combine.sh
@@ -0,0 +1,9 @@
+# python combine.py \
+#     --model_name CodeLlama-13b-hf \
+#     --data_version v1.3 \
+#     --ckpt 500
+
+python combine.py \
+    --model_name CodeLlama-13b-hf \
+    --data_version v1.4 \
+    --ckpt 500
