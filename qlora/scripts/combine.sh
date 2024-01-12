6586ea3b6d27005239ac340b8e111ee72cb1f503
jiaqiLv <1031903858@qq.com>
2024-01-12 08:06:40 +0000

    perfect shell scripts

--- qlora/scripts/combine.sh
+++ qlora/scripts/combine.sh
@@ -1,9 +1,4 @@
-# python combine.py \
-#     --model_name CodeLlama-13b-hf \
-#     --data_version v1.3 \
-#     --ckpt 500
-
 python combine.py \
     --model_name CodeLlama-13b-hf \
-    --data_version v2.0_without_ir \
-    --ckpt 500
+    --data_version v2.1_simplify_nn_topi_without_ir \
+    --ckpt 50
--- qlora/scripts/performance_test.sh
+++ qlora/scripts/performance_test.sh
@@ -8,25 +8,25 @@
 # 更改test_data_version更换测试集
 
 ####1####
-python performance_test.py \
-    --model_name CodeLlama-13b-hf \
-    --train_data_version v2.0_without_ir \
-    --test_data_version test \
-    --ckpt 500 \
-    --device 0 \
-    --max_length 50 \
-    --use_lora_model True \
-    --use_alpaca_prompt True \
-    --bits 8 \
-    
-####2####
 # python performance_test.py \
 #     --model_name CodeLlama-13b-hf \
-#     --train_data_version v1.5_with_ir \
-#     --test_data_version v1.5_with_ir \
+#     --train_data_version v2.0_without_ir \
+#     --test_data_version v2.0_without_ir \
 #     --ckpt 500 \
-#     --device 1 \
-#     --max_length 500 \
+#     --device 0 \
+#     --max_length 3000 \
 #     --use_lora_model True \
 #     --use_alpaca_prompt True \
 #     --bits 8 \
+    
+####2####
+python performance_test.py \
+    --model_name CodeLlama-13b-hf \
+    --train_data_version v2.1_simplify_nn_topi_without_ir \
+    --test_data_version test \
+    --ckpt 50 \
+    --device 1 \
+    --max_length 500 \
+    --use_lora_model True \
+    --use_alpaca_prompt True \
+    --bits 8 \
