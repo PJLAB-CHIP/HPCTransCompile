aa66cf024df5e90da7a927c20e3858142663195c
jiaqiLv <1031903858@qq.com>
2024-03-08 07:49:56 +0000

    feat(scripts):scripts execution files

---
+++ scripts/finetune.sh
@@ -0,0 +1,36 @@
+# python finetune.py \
+#     --model_name_or_path /code/model/CodeLlama-13b-hf \
+#     --output_dir output/hpc_v3.0_topi_without_ir_by_name_simplify \
+#     --dataset_name_or_path /code/LLM4HPCTransCompile/preprocess/data/hpc_v3.0_topi_without_ir_by_name_simplify/hpc_train_v3.0_topi_without_ir_by_name_simplify.json \
+#     --dataset_format input-output \
+#     --use_wandb True \
+#     --wandb_run_name hpc_v3.0_topi_without_ir_by_name_simplify \
+#     --device_map cuda:0 \
+#     --full_finetune False \
+#     --source_max_len 512 \
+#     --target_max_len 512 \
+
+#### Llama-2-7b-hf ####
+python finetune.py \
+    --model_name_or_path /code/model/Llama-2-7b-hf \
+    --output_dir output/hpc_v3.0_topi_without_ir_by_name_simplify \
+    --dataset_name_or_path /code/LLM4HPCTransCompile/preprocess/data/hpc_v3.0_topi_without_ir_by_name_simplify/hpc_train_v3.0_topi_without_ir_by_name_simplify.json \
+    --dataset_format input-output \
+    --use_wandb True \
+    --wandb_run_name hpc_v3.0_topi_without_ir_by_name_simplify \
+    --device_map auto \
+    --full_finetune True \
+    --batch_size 1 \
+    --source_max_len 512 \
+    --target_max_len 512 \
+
+
+#### test ####
+# python finetune.py \
+#     --model_name_or_path /code/model/CodeLlama-13b-hf \
+#     --output_dir output/hpc_test \
+#     --dataset_name_or_path /code/LLM4HPCTransCompile/preprocess/data/hpc_test/hpc_train_test.json \
+#     --dataset_format input-output \
+#     --use_wandb True \
+#     --wandb_run_name hpc_test \
+#     --device_map cuda
