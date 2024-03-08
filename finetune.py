0d5e9968cae85f7c26420b9b682196649fe9fa03
jiaqiLv <1031903858@qq.com>
2024-03-08 07:51:15 +0000

    feat(*):init functions of fine-tune, combine model and performance test

---
+++ finetune.py
@@ -0,0 +1,264 @@
+from ast import arg
+from curses import meta
+import logging
+import argparse
+import os
+import sys
+from dataclasses import dataclass,field
+from time import sleep
+from unittest import result
+from git import Optional, Union
+from transformers import (
+    LlamaForCausalLM,
+    LlamaTokenizer,
+    Seq2SeqTrainer,
+    CodeLlamaTokenizer,
+    BitsAndBytesConfig
+)
+from peft import (
+    LoraConfig,
+    get_peft_model,
+    prepare_model_for_kbit_training,
+    get_peft_model_state_dict
+)
+import transformers
+from datasets import load_dataset
+from os.path import (
+    isdir,
+    join,
+    exists
+)
+from utils import (
+    show_model_info,
+    extract_alpaca_dataset
+)
+from args import(
+    ModelArguments,
+    DataArguments,
+    TrainingArguments
+)
+from typing import Optional,Sequence,Dict
+import torch
+from torch.nn.utils.rnn import pad_sequence
+import copy
+import subprocess
+from torch import optim
+
+# os.environ['WANDB_API_KEY'] = 'cfb5ba8f1bb02b39b518c24874b8579617459db3'
+# os.environ['WANDB_MODE'] = 'offline'
+
+IGNORE_INDEX = -100
+
+# TODO: 进一步完善DataCollator模块
+@dataclass
+class DataCollatorForCausalLM(object):
+    tokenizer: transformers.PreTrainedTokenizer
+    source_max_len: int
+    target_max_len: int
+    train_on_source: bool
+    predict_with_generate: bool
+
+    def __call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
+        # Extract elements
+        
+        sources = [f"{self.tokenizer.bos_token}{example['input']}" for example in instances]
+        targets = [f"{example['output']}{self.tokenizer.eos_token}" for example in instances]
+        # Tokenize
+        tokenized_sources_with_prompt = self.tokenizer(
+            sources,
+            max_length=self.source_max_len,
+            truncation=True,
+            add_special_tokens=False,
+        )
+        tokenized_targets = self.tokenizer(
+            targets,
+            max_length=self.target_max_len,
+            truncation=True,
+            add_special_tokens=False,
+        )
+        # Build the input and labels for causal LM
+        input_ids = []
+        labels = []
+        for tokenized_source, tokenized_target in zip(
+                tokenized_sources_with_prompt['input_ids'],
+                tokenized_targets['input_ids']
+        ):
+            if not self.predict_with_generate:
+                input_ids.append(torch.tensor(tokenized_source + tokenized_target))
+                if not self.train_on_source:
+                    labels.append(
+                        torch.tensor(
+                            [IGNORE_INDEX for _ in range(len(tokenized_source))] + copy.deepcopy(tokenized_target))
+                    )
+                else:
+                    labels.append(torch.tensor(copy.deepcopy(tokenized_source + tokenized_target)))
+            else:
+                input_ids.append(torch.tensor(tokenized_source))
+        # Apply padding
+        input_ids = pad_sequence(input_ids, batch_first=True, padding_value=self.tokenizer.pad_token_id)
+        labels = pad_sequence(labels, batch_first=True,
+                              padding_value=IGNORE_INDEX) if not self.predict_with_generate else None
+        data_dict = {
+            'input_ids': input_ids,
+            'attention_mask': input_ids.ne(self.tokenizer.pad_token_id),
+        }
+        if labels is not None:
+            data_dict['labels'] = labels
+        return data_dict
+
+
+
+def get_data_module(args):
+    def format_dataset(dataset):
+        """根据数据格式对input和output进行填充形成完整的prompt"""
+        if args.dataset_format == 'alpaca' or args.dataset_format == 'alpaca-clean' or (args.dataset_format is None and args.dataset_name_or_path in ['alpaca','alpaca-clean']):
+            dataset = dataset.map(extract_alpaca_dataset,remove_columns=['instruction','op_name'])
+        elif args.dataset_format == 'input-output':
+            pass
+        return dataset
+    
+    def generate_and_tokenize_prompt(data):
+        data['length'] = len(data['input'])+len(data['output'])
+        return data
+    
+    if args.dataset_name_or_path.endswith('.json') or args.dataset_name_or_path.endswith('.jsonl'):
+        data = load_dataset('json',data_files=args.dataset_name_or_path)
+    else:
+        data = load_dataset(args.dataset_name_or_path)
+    
+    data = format_dataset(data)
+        
+    # train|val|test划分
+    data = data['train'].train_test_split(train_size=args.train_size,seed=42)
+    print('data[\'train\']:', data['train'])
+    print('data[\'test\']:', data['test'])
+    train_data = data['train'].shuffle().map(generate_and_tokenize_prompt)
+    val_data = data['test'].shuffle().map(generate_and_tokenize_prompt)
+    return train_data,val_data
+
+def get_accelerate_model(args):
+    if not args.full_finetune:  
+        model = LlamaForCausalLM.from_pretrained(
+            pretrained_model_name_or_path = args.model_name_or_path,
+            device_map=args.device_map,
+            load_in_4bit = (not args.full_finetune) and args.bits == 4,
+            load_in_8bit = (not args.full_finetune) and args.bits == 8,
+            quantization_config=BitsAndBytesConfig(
+                load_in_4bit = (not args.full_finetune) and args.bits == 4,
+                load_in_8bit = (not args.full_finetune) and args.bits == 8,
+                bnb_4bit_quant_type=args.quant_type,
+                bnb_4bit_use_double_quant=args.double_quant,
+                bnb_4bit_compute_dtype=torch.bfloat16
+            ),
+        ) 
+        model = prepare_model_for_kbit_training(model) # wraps the entire protocol for preparing a model before running a training.
+        config = LoraConfig(
+            r = 8,
+            lora_alpha = 16,
+            target_modules = ['q_proj','k_proj','v_proj'],
+            lora_dropout = 0.05,
+            bias = "none",
+            task_type = "CAUSAL_LM",
+        ) # target_modules控制在哪些层加LORA
+        model = get_peft_model(model,config) # LlamaForCausalLM --> PeftModelForCausalLM
+    else:
+        model = LlamaForCausalLM.from_pretrained(
+        pretrained_model_name_or_path = args.model_name_or_path,
+        device_map=args.device_map,
+        )
+
+
+    checkpoint_dir,completed_training = get_last_checkpoint(args.output_dir)
+    # TODO: 从checkpoint加载模型
+    if not completed_training:
+        pass
+    tokenizer = CodeLlamaTokenizer.from_pretrained(
+        pretrained_model_name_or_path = args.model_name_or_path,
+        padding_side = 'left'
+    )
+    tokenizer.pad_token = tokenizer.eos_token
+    return model,tokenizer
+
+def get_last_checkpoint(checkpoint_dir):
+    if isdir(checkpoint_dir):
+        is_completed = exists(join(checkpoint_dir,'completed'))
+        if is_completed:
+            return None,True
+        max_step = 0
+        for folder_name in os.listdir(checkpoint_dir):
+            if isdir(join(checkpoint_dir,folder_name)) and folder_name.startswith('checkpoint'):
+                max_step = max(max_step,int(folder_name.replace('checkpoint-','')))
+        if max_step == 0:
+            return None, is_completed
+        checkpoint_dir = join(checkpoint_dir,f'checkpoint-{max_step}')
+        print(f'Find a previous checkpoint at: {checkpoint_dir}')
+        return checkpoint_dir,is_completed
+    else:
+        raise ValueError(f'Expected checkpoint_dir be a dir, but get {checkpoint_dir}')
+
+def train():
+    hfparser = transformers.HfArgumentParser(
+        (ModelArguments,DataArguments,TrainingArguments)
+    )
+    model_args,data_args,training_args,extra_args = hfparser.parse_args_into_dataclasses(return_remaining_strings=True)
+
+    args = argparse.Namespace(
+        **vars(model_args),**vars(data_args),**vars(training_args)
+    )
+    print('args:', args)
+    if not exists(path=args.output_dir):
+        os.mkdir(args.output_dir)
+    model, tokenizer = get_accelerate_model(args)
+    train_data,val_data = get_data_module(args)
+    # print('train_data:', train_data)
+    # print('val_data:', val_data)
+    # optimizer = optim.SGD(model.parameters(),lr=args.learning_rate)
+    trainer = transformers.Trainer(
+        model=model,
+        train_dataset=train_data,
+        eval_dataset=val_data,
+        args=transformers.TrainingArguments(
+            per_device_train_batch_size=args.batch_size,
+            gradient_accumulation_steps=1,
+            warmup_steps=100,
+            num_train_epochs=args.num_epochs,
+            learning_rate=args.learning_rate,
+            fp16=True,
+            logging_steps=10,
+            optim="adamw_torch",
+            evaluation_strategy="steps",
+            save_strategy="steps",
+            eval_steps=args.eval_steps,
+            save_steps=args.save_steps,
+            output_dir=args.output_dir,
+            save_total_limit=3,
+            load_best_model_at_end=True,
+            group_by_length=args.group_by_length,
+            report_to="wandb" if args.use_wandb else None,
+            run_name=args.wandb_run_name if args.use_wandb else None,
+        ),
+        data_collator = DataCollatorForCausalLM(
+            tokenizer=tokenizer,
+            source_max_len=args.source_max_len,
+            target_max_len=args.target_max_len,
+            train_on_source=False,
+            predict_with_generate=False,
+        ),
+    )
+    model.config.use_cache = False
+
+    # old_state_dict = model.state_dict
+    # model.state_dict = (
+    #     lambda self, *_, **__: get_peft_model_state_dict(
+    #         self, old_state_dict()
+    #     )
+    # ).__get__(model, type(model))
+
+    if torch.__version__ >= "2" and sys.platform != "win32":
+        model = torch.compile(model)
+    
+    trainer.train()
+    model.save_pretrained(args.output_dir)
+
+if __name__ == '__main__':
+    train()
---
+++ performance_test.py
@@ -0,0 +1,240 @@
+from ast import Tuple
+from re import split
+import defusedxml
+from sympy import sequence
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    LlamaForCausalLM
+)
+from transformers.generation import GenerationConfig
+import torch
+import os
+import json
+import tqdm
+import argparse
+import logging
+from utils import *
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn.functional as F
+import random
+import codebleu
+
+"""命令行解析参数信息"""
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_name',type=str,help='foundation model')
+parser.add_argument('--train_data_version',type=str)
+parser.add_argument('--test_data_version',type=str)
+parser.add_argument('--ckpt',type=int)
+parser.add_argument('--device',type=int,default=0)
+parser.add_argument('--use_lora_model',type=bool,default=False,help='使用fine-tune后的模型还是基座模型')
+parser.add_argument('--prompt_format',type=str,default='none',help='prompt模板样式[none|common|alpaca]')
+parser.add_argument('--quantize_model',type=bool,default=False)
+parser.add_argument('--bits',type=int,default=8)
+parser.add_argument('--max_length',type=int,default=3000)
+parser.add_argument('--extra_length',type=int,default=100)
+parser.add_argument('--match_output',type=bool,default=False,help='是否将输出长度匹配gt长度')
+args = parser.parse_args()  # 解析命令行参数
+
+print('args:', args)
+
+# 路径信息
+if args.use_lora_model:
+    MODEL_PATH = f"./save_model/{args.model_name}-{args.ckpt}-{args.train_data_version}"
+else:
+    MODEL_PATH = f"../../model/{args.model_name}"
+EVAL_DATA_PATH = f"../preprocess/data/hpc_{args.test_data_version}/hpc_test_{args.test_data_version}.json"
+MODEL_IDENTIFY = f'{args.model_name}-{args.ckpt}-{args.train_data_version}-{args.test_data_version}'
+
+# prompt模板
+EVAL_PROMPT = "{}\n\n### Input:\n{}\n\n### Response: "
+ALPACA_PROMPT_DICT = {
+    "prompt_input": (
+        "Below is an instruction that describes a task, paired with an input that provides further context. "
+        "Write a response that appropriately completes the request.\n\n"
+        "### Instruction:\n{}\n\n### Input:\n{}\n\n### Response: "
+    ),
+    "prompt_no_input": (
+        "Below is an instruction that describes a task. "
+        "Write a response that appropriately completes the request.\n\n"
+        "### Instruction:\n{}\n\n### Response: "
+    ),
+}
+
+def load_eval():
+    """
+    加载测试集数据
+    """
+    with open(EVAL_DATA_PATH, 'r') as file:
+        eval_datas = json.load(file)
+    sequences_info = []
+    for index,eval_data in enumerate(eval_datas):
+        sequence_info = {}
+        if args.prompt_format == 'alpaca':
+            sequence = ALPACA_PROMPT_DICT['prompt_input'].format(eval_data['instruction'],eval_data['input'])
+        elif args.prompt_format == 'common':
+            sequence = EVAL_PROMPT.format(eval_data['instruction'],eval_data['input'])
+        elif args.prompt_format == 'none':
+            sequence = eval_data['input']
+        else:
+            raise ValueError(f'{args.prompt_format} is illegal')
+        sequence_info["sequence"] = sequence
+        sequence_info["gt"] = eval_data['output']
+        sequence_info["op_name"] = eval_data['op_name']+"_{}".format(index)
+        sequences_info.append(sequence_info)
+    return sequences_info
+
+
+def show_attention_matrix(attention_tuple: Tuple, input_token_num: int, generate_token_num: int, op_name: str):
+    def draw_matrix(matrix, mode='layer',**kwargs):
+        plt.imshow(matrix)
+        # 添加颜色条
+        cbar = plt.colorbar()
+        # 添加颜色条标签
+        cbar.set_label('Colorbar Label')
+        if mode == 'layer':
+            i = kwargs['layer_num']
+            plt.title(f'{op_name}:attention_layer_{i}')
+            plt.savefig(f'./pictures/{MODEL_IDENTIFY}/{op_name}/attention_layer_{i}')
+        elif mode == 'mean':
+            plt.title(f'{op_name}:attention_layer_mean')
+            plt.savefig(f'./pictures/{MODEL_IDENTIFY}/{op_name}/attention_layer_mean')
+        elif mode == 'sum&softmax':
+            plt.title(f'{op_name}:attention_layer_sum&softmax')
+            plt.savefig(f'./pictures/{MODEL_IDENTIFY}/{op_name}/attention_layer_sum&softmax')
+        cbar.remove()
+    
+    attention_list = []
+    for lines in attention_tuple:
+        attention_list.append(lines[0][0].cpu().float())
+        # print('--len(lines):', len(lines))
+        # for line in lines:
+        #     print('----len(line):', len(line))
+    print('len(attention_list):', len(attention_list))
+
+    total_token_num = input_token_num + generate_token_num  # 总token数
+    dim_0 = attention_list[0].shape[0]
+    attention_matrix = torch.zeros(dim_0,total_token_num,total_token_num)
+
+    cur_row = 0 # 当前需要写入的行
+    for i,_item in enumerate(attention_list):
+        # print('_item.shape:', _item.shape)
+        # print(i,i+_item.shape[1],i+_item.shape[2])
+        attention_matrix[:,cur_row:cur_row+_item.shape[1],:_item.shape[2]] = _item
+        cur_row += _item.shape[1]
+
+    print('attention_matrix.shape:', attention_matrix.shape)
+    """展示模型inference计算生成的attention矩阵"""
+    """PART1 每个layer的attention"""
+    if not os.path.exists(f'./pictures/{MODEL_IDENTIFY}'):
+        os.mkdir(f'./pictures/{MODEL_IDENTIFY}')
+    if not os.path.exists(f'./pictures/{MODEL_IDENTIFY}/{op_name}'):
+        os.mkdir(f'./pictures/{MODEL_IDENTIFY}/{op_name}')
+    for i,sub_matrix in enumerate(attention_matrix):
+        draw_matrix(sub_matrix,mode='layer',layer_num=i)
+    """PART2 求所有层的mean和sum-->sotfmax"""
+    sum_matrix = torch.sum(attention_matrix,dim=0)
+    sum_matrix = F.softmax(sum_matrix,dim=1)
+    mean_matrix = torch.mean(attention_matrix,dim=0)
+    draw_matrix(sum_matrix,mode='sum&softmax')
+    draw_matrix(mean_matrix,mode='mean')
+
+def show_model_info(model):
+    layer_list = list(model.state_dict().keys())
+    print('layer_list:', layer_list)
+
+def cal_codebleu(prediction,reference):
+    result = codebleu.calc_codebleu([reference],[prediction],lang='c',weights=(0.25,0.25,0.25,0.25),tokenizer=None)
+    print(result)
+    return result
+
+def generate(sequences_info):
+    """1. 加载model和tokenizer"""
+    device_map = f"cuda:{args.device}"
+    model = LlamaForCausalLM.from_pretrained(
+        MODEL_PATH,
+        torch_dtype=torch.bfloat16,
+        device_map=device_map,
+        attn_implementation="eager",
+        load_in_4bit= args.quantize_model and args.bits==4,
+        load_in_8bit= args.quantize_model and args.bits==8,
+        )
+    model = model.eval()
+    """计算模型参数量"""
+    # cal_model_params(model)
+    """2. Generate"""
+    diff = []  # generate和gt不同的op
+    tokenizer = AutoTokenizer.from_pretrained(
+        MODEL_PATH,
+        padding_side='left'
+    )
+    
+    random.shuffle(sequences_info)
+    for sequence_info in tqdm.tqdm(sequences_info,total=len(sequences_info)):
+        """2.1. 对输入进行编码&inference"""
+        # tokens = tokenizer.tokenize(sequence_info["sequence"])
+        input_ids = tokenizer.encode(sequence_info["sequence"], return_tensors="pt").to(f'cuda:{args.device}')
+        if args.match_output:
+            gt_ids = tokenizer.encode(sequence_info["gt"],return_tensors='pt')
+            print('len(input_ids[0])+len(gt_ids[0]):', len(input_ids[0])+len(gt_ids[0]))
+        print('len(input_ids):', len(input_ids[0]))
+        with torch.no_grad():
+            outputs = model.generate(
+                input_ids=input_ids, 
+                generation_config = GenerationConfig(
+                    return_dict_in_generate = True,
+                    output_attentions = True,
+                    output_scores = True
+                ),
+                max_length=args.max_length if not args.match_output else (len(input_ids[0])+len(gt_ids[0])+args.extra_length), 
+                num_beams=3,
+                )
+        """2.2. 对输出进行解码,提取输出序列和attention矩阵"""
+        generated_text = tokenizer.decode(outputs.sequences[0], skip_special_tokens=True)
+        attention_tuple = outputs.attentions
+        print('attention_tuple.shape:', len(attention_tuple))
+        # show_attention_matrix(attention_tuple,len(input_ids[0]),len(attention_tuple),sequence_info["op_name"]) # 绘制attention热力图
+        # generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        """extra: 计算generate和gt的CodeBLEU"""
+        # generate = generated_text.split("### Response:  ")[-1]
+        generate = generated_text.lstrip(sequence_info['sequence'])
+        gt = sequence_info["gt"]
+        score = cal_codebleu(generate,gt)
+        
+        # 检查路径信息
+        if not os.path.exists(f'./evaluate/{MODEL_IDENTIFY}'):
+            os.mkdir(f'./evaluate/{MODEL_IDENTIFY}')
+            os.mkdir(f'./evaluate/{MODEL_IDENTIFY}/generate')
+            os.mkdir(f'./evaluate/{MODEL_IDENTIFY}/gt')
+            os.mkdir(f'./evaluate/{MODEL_IDENTIFY}/full_gen')
+        
+        with open(os.path.join('evaluate',MODEL_IDENTIFY,'codebleu.txt'),'a') as file:
+            file.write('{}:{}\n'.format(sequence_info["op_name"],score))
+        if generate != gt:
+            diff.append(sequence_info["op_name"])
+        """3. 保存generate和对应的gt"""
+        # with open("./evaluate/generate/{}_generate.c".format(sequence_info["op_name"]), 'w') as file:
+        # print('generate:', generate)
+        with open(os.path.join(f'./evaluate/{MODEL_IDENTIFY}','generate',"{}_generate.c".format(sequence_info["op_name"])),'w') as file:
+            file.write(generate)
+        with open(os.path.join(f'./evaluate/{MODEL_IDENTIFY}','gt',"{}_gt.c".format(sequence_info["op_name"])),'w') as file:
+            file.write(gt)
+        with open(os.path.join(f'./evaluate/{MODEL_IDENTIFY}','full_gen',"{}_full_gen.c".format(sequence_info["op_name"])),'w') as file:
+            file.write(generated_text)
+    print('diff:', diff)
+    return diff
+
+
+
+if __name__ == "__main__":
+    sequences_info = load_eval()  # 数据加载
+    diff_op = generate(sequences_info=sequences_info)
+    """数据集日志信息LOG"""
+    logging.basicConfig(filename=os.path.join(f'./evaluate/{MODEL_IDENTIFY}','evaluate_model_info.log'), level=logging.INFO, filemode='w+')  # 日志信息
+    logging.info('MODEL_PATH: %s', MODEL_PATH)
+    logging.info('EVAL_DATA_PATH: %s', EVAL_DATA_PATH)
+    logging.info('EVAL_PROMPT: %s', EVAL_PROMPT)
+    logging.info('diff_op: %s', diff_op)
+
